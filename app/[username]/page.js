0d761a2769f90eacc42b86f2af6e094dfde7bcdf
Nikesh <kirthanbirwaz43@gmail.com>
2025-01-03 18:07:29 +0530

    Update page.js

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { notFound } from "next/navigation"
 import Paymentpage from "@/components/Paymentpage";
 import connectDb from "@/db/dbConnect";
 import User from "@/models/User";
 const Username = async ({ params }) => {
   const a=await params
   const username = a.username;
- 
+ const u= await User.findOne({"username":username})
+  if(!u){
+     return notFound();
+  }
   return (
     <>
       <Paymentpage username={username} />
